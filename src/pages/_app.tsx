806d098fc829a9c35ebbbcfd472d1026126e8a51
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2024-03-15 23:29:24 -0500

    Initialization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRef } from "react";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import "@/styles/locomotive.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const containerRef = useRef(null);
+
+  return (
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+        // ... all available Locomotive Scroll instance options
+      }}
+      watch={[]}
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        <Component {...pageProps} />
+      </main>
+    </LocomotiveScrollProvider>
+  );
 }
